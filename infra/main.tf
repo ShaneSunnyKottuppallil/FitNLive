9cec0fae278baf46ef36ffdf09c69b060fd28779
Shane Kottuppallil <ssk@Shane-Sunnys-Macbook-Pro.local>
2026-04-27 19:44:19 +0530

    CICD Test

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket  = "mern-devops-tf-state-major"
-    key     = "terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 # Default VPC
 data "aws_vpc" "default" {
   default = true
