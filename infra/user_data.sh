280639810229ff5215f8f4b56d4882772be0ca75
Shane Kottuppallil <ssk@Shane-Sunnys-Macbook-Pro.local>
2026-04-28 19:20:01 +0530

    CICD Test

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -2,32 +2,28 @@
 set -e
 
 # Update system
-yum update -y
+apt-get update -y
 
 # Install Docker + Git
-yum install -y docker git
+apt-get install -y docker.io git
 
 # Start Docker
 systemctl start docker
 systemctl enable docker
 
-# Allow ec2-user to use Docker
-usermod -aG docker ec2-user
+# Allow ubuntu user to use Docker
+usermod -aG docker ubuntu
 
-# Install Docker Compose (v1 standalone)
+# Install Docker Compose
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
--o /usr/local/bin/docker-compose
+  -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
-# Fix permissions immediately (no logout needed)
-newgrp docker <<EONG
-docker version
-EONG
-
-# Create app directory (CI/CD will use this)
-mkdir -p /home/ec2-user/FitInLive
+# Create app directory
+mkdir -p /home/ubuntu/FitNLive
 
 # Set ownership
-chown -R ec2-user:ec2-user /home/ec2-user/FitInLive
+chown -R ubuntu:ubuntu /home/ubuntu/FitNLive
+
 echo "✅ EC2 setup complete"
